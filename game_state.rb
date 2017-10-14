1bd61d9fc58e160c47bf43054f97f244846346c8
John Martinsson <john.martinsson@gmail.com>
2017-10-14 22:40:32 +0200

    Fix indentation

--- game_state.rb
+++ game_state.rb
@@ -6,41 +6,43 @@ require_relative 'player'
 
 
 class GameState
-    def initialize(width, height)
+  def initialize(width, height)
 
-        spawn_entity = Proc.new do |*args|
-          @entities << args.first
-        end
-
-        @spawners = [
-          Spawner.new(5000, 16.6, width, height, spawn_entity),
-        ]
-        @background = Background.new(0,0)
-        @entities = [
-          Player.new(50, height/2)
-        ]
+    spawn_entity = Proc.new do |*args|
+      @entities << args.first
     end
 
-    def update
-        @spawners.each do |s|
-            s.update()
-          end
+    @spawners = [
+      Spawner.new(5000, 16.6, width, height, spawn_entity),
+    ]
 
-          @background.update()
-          @entities.delete_if do |e|
-            if e.removeMe
-              true
-            else
-              e.update()
-              false
-            end
-          end
+    @background = Background.new(0,0)
+    @entities = [
+      Player.new(50, height/2)
+    ]
+  end
+
+  def update
+    @spawners.each do |s|
+      s.update()
     end
 
-    def draw
-        @background.draw()
-        for e in @entities
-          e.draw()
-        end
+    @background.update()
+
+    @entities.delete_if do |e|
+      if e.removeMe
+        true
+      else
+        e.update()
+        false
+      end
     end
+  end
+
+  def draw
+    @background.draw()
+    for e in @entities
+      e.draw()
+    end
+  end
 end
