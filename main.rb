855c31598ff53271b329d7186ef3df8e1ca6dea9
John Martinsson <john.martinsson@gmail.com>
2017-10-14 14:43:19 +0200

    Add main game loop

---
+++ main.rb
@@ -0,0 +1,18 @@
+require 'gosu'
+
+class StartDestroyer < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "StartDestroyer Game"
+  end
+
+  def update
+    # ...
+  end
+
+  def draw
+    # ...
+  end
+end
+
+StartDestroyer.new.show
